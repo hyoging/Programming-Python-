43cb0472983a9aaef5297e3b468d926600e37841
User <w2030@e-mirim.hs.kr>
2021-09-13 13:55:40 +0900

    invalidlenfthExrror 연습

--- module,package/tour/baseball.py
+++ module,package/tour/baseball.py
@@ -1,14 +1,21 @@
 from baseball_game_engine import make_answer, check
-from random import random
-# 정답 만들자: 0~9 숫자 세개 뽑자
-def make_answer():
-    list_r = random.sample(range(9+1),3)
-    return "".join(str, list_r)
+from custom_error import InvalidLengthErrork
 
-def check(guess, answer):
-    strike = 0
-    ball = 0
+answer = make_answer()
 
-    # 숫자 하나 꺼내서 정답에 있고, 자리가 같으면, strike += 1
-    # 숫자 하나 꺼내서 정답에 있고, 자리가 다르면, ball += 1
-    guess[0] in answer
+while True:
+    guess = input("뭘까?")
+    # 숫자인지 아닌지
+    try:
+        guess_int = int(guess)
+    except ValueError as e:
+        print('숫자를 입력하세요')
+        continue
+    if len(guess) != len(answer)
+        raise InvalidLengthExrror('정답의 길이와 다른 것을 입력했네요')
+
+    strike, ball = check(guess, answer)
+    print(f'{guess}\tstrike: {strike}, ball: {ball}')
+    if answer == guess:
+        print("정답입니다.")
+        break
--- module,package/tour/baseball_game_engine.py
+++ module,package/tour/baseball_game_engine.py
@@ -1,45 +1,32 @@
 import random
-# 정답 만들자: 0~9 숫자 세개 뽑자
 def make_answer():
-    list_r = random.sample(range(9+1),3)
-    return "".join(str, list_r)
+    answer = random.sample(range(0,9,1),3)
+    return  "".join(str(n) for n in answer)
+
 
 def check(guess, answer):
     strike = 0
     ball = 0
-
+    #숫자 하나 꺼내서 정답에 있고, 자리가 같으면 strike += 1
+    #숫자 하나 꺼내서 정답에 있고, 자리가 다르면, ball += 1
     for i, g in enumerate(guess):
-        for j, a in range(answer):
-            if g == a:  # 숫자가 같으면
-                if i == j:  # 자리가 같으면
+        for j, a in enumerate(answer):
+            if g == a:  #숫자가 같으면
+                if i == j:  #자리가 같으면
                     strike += 1
-                else:
+                else:       #자리가 다르면
                     ball += 1
-
-    return strike, ball
+    return strike,ball
 
 
 if __name__ == '__main__':
+
     answer = make_answer()
     print(answer)
-    strike, ball = check("832", "934")
-    print(strike, ball)
-    strike, ball = check("431", "934")
-    print(strike, ball)
-    strike, ball = check("934", " 934")
-    print(strike, ball)
-answer = make_answer()
-
-answer = make_answer()
-# 무한반복
-while True:
-# 숫자 묻자
-    guess = input("뭘까요~?")
-# strike, ball 판정하자
-    strike, ball = check(guess, answer)
-# 출력하자
-    print(f'{guess}\tstrike: {strike}, ball: {ball}')
-# 정답 == 숫자, 끝내자
-    if answer == guess:
-        print('정답입니다.')
-        break
+    check()
+    strike, ball = check("831","934")
+    print(strike, ball)     #1 0
+    strike, ball = check("431","934")
+    print(strike, ball)     #1 1
+    strike, ball = check("934","934")
+    print(strike, ball)     #3 0
