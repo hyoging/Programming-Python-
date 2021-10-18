bb57f764060109818152f3c3cd28477477280737
User <w2030@e-mirim.hs.kr>
2021-10-18 12:12:24 +0900

    count try

--- module,package/tour/baseball.py
+++ module,package/tour/baseball.py
@@ -1,8 +1,8 @@
 from baseball_game_engine import make_answer, check
-from custom_error import InvalidLengthErrork
+from custom_error import InvalidLengthError
 
 answer = make_answer()
-
+count = 0
 while True:
     guess = input("뭘까?")
     # 숫자인지 아닌지
@@ -11,11 +11,13 @@ while True:
     except ValueError as e:
         print('숫자를 입력하세요')
         continue
-    if len(guess) != len(answer)
-        raise InvalidLengthExrror('정답의 길이와 다른 것을 입력했네요')
-
+    if len(guess) != len(answer):
+        raise InvalidLengthError('정답의 길이와 다른 것을 입력했네요')
+        print(f'정답의 길이와 다른 것을 입력했네요.{len(answer)}문자')
+        continue
     strike, ball = check(guess, answer)
-    print(f'{guess}\tstrike: {strike}, ball: {ball}')
+    count += 1
+    print(f'{guess}\tstrike: {strike}, ball: {ball}\t+{count}+try')
     if answer == guess:
         print("정답입니다.")
         break
